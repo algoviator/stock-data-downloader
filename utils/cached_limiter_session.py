638a19c6dcbe45c1996dbacb899a4c34b01f35b3
algoviator <algoviator@gmail.com>
2024-01-11 20:38:42 +0200

    Add cached_limiter_session.py into utils dir

---
+++ utils/cached_limiter_session.py
@@ -0,0 +1,32 @@
+# --------------------------------------- HEADER -------------------------------------------
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024 Roma Usoltsev
+# https://www.linkedin.com/in/algoviator/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# --------------------------------------- IMPORT LIBRARIES -------------------------------------------
+
+# Requests rate limiter
+from requests import Session
+from requests_cache import CacheMixin, SQLiteCache
+from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
+from pyrate_limiter import Duration, RequestRate, Limiter
+
+# --------------------------------------- CACHED LIMITER SESSION CLASS ---------------------------------
+
+class CachedLimiterSession(CacheMixin, LimiterMixin, Session):
+    """
+    Requests rate limiter session with caching capabilities.
+    """
+    pass
