a4bd4af6997b71becdadab1d15779d282f864d83
algoviator <algoviator@gmail.com>
2024-01-15 15:26:45 +0200

    fix: resolve issue with no columns 'dividends' and 'splits' in df

--- quotes_downloader.py
+++ quotes_downloader.py
@@ -251,15 +251,20 @@ class QuotesDownloader:
         try:
             df = df.astype(self.COLS_TYPES)
         except Exception as e:
-            # sometimes there is no column 'dividends' in 'df' DataFrame
-            if 'dividends' in self.COLS_TYPES:
-                new_cols_types = self.COLS_TYPES.copy()
-                del new_cols_types['dividends']
-                try:
-                    df = df.astype(new_cols_types)
-                except Exception as e:
-                    # Log any exceptions
-                    self.logger.error(f'refactor_dataframe_columns() - df.astype(), Error: {e}', exc_info=True)
+            # sometimes there are no columns 'dividends' and 'splits' in 'df' DataFrame
+
+            # remove missed columns from current types to apply
+            new_cols_types = self.COLS_TYPES.copy()
+            keys_to_remove = ['dividends', 'splits']
+            for key in keys_to_remove:
+                del new_cols_types[key]
+
+            # and try again
+            try:
+                df = df.astype(new_cols_types)
+            except Exception as e:
+                # Log any exceptions
+                self.logger.error(f'refactor_dataframe_columns() - df.astype(), Error: {e}', exc_info=True)
 
         return df
 
